222b906ef99c191bdf749dc02a1a676e3d5de951
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-04-24 16:44:48 -0300

    add: criando arquivo data para armazenar textos, imagens, titulos, e descriçoes que podem ser alterada via base de dados, sem mexer nos códigos.

---
+++ src/data/SobreMimData.jsx
@@ -0,0 +1,57 @@
+
+import IconeDev from "../iconeDev.svg";
+import IconeMais from "../iconeMais.svg";
+import IconeHtml from "../iconeHtml.svg";
+import IconeCss from "../iconeCss.svg";
+import IconeJs from "../iconeJavaScript.svg";
+import IconeReact from "../iconeReact.svg";
+
+const SobreMimData = {
+    titulo: "Sobre Mim",
+    descricao: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque laborum provident consequuntur facilis inventore quis perspiciatis enim et dolorem deserunt animi assumenda, doloremque, vero, officiis nisi commodi alias architecto nam?",
+    
+    atividades: [
+      {
+        id: 1,
+        titulo: "Web Design",
+        icone: IconeDev,
+        descricao: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nesciunt possimus perferendis dolores, re"
+      }
+    ],
+    
+    tecnologias: [
+      {
+        id: 1,
+        nome: "HTML",
+        icone: IconeHtml
+      },
+      {
+        id: 2,
+        nome: "CSS",
+        icone: IconeCss
+      },
+      {
+        id: 3,
+        nome: "JavaScript",
+        icone: IconeJs
+      },
+      {
+        id: 4,
+        nome: "React",
+        icone: IconeReact
+      }
+    ],
+    
+    futurasIdeias: [
+      {
+        id: 1,
+        icone: IconeMais
+      },
+      {
+        id: 2,
+        icone: IconeMais
+      }
+    ]
+  };
+  
+  export default SobreMimData;
--- src/pages/SobreMim.jsx
+++ src/pages/SobreMim.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./SobreMim.css";
-import IconeDev from "../iconeDev.svg";
-import IconeMais from "../iconeMais.svg";
-import IconeHtml from "../iconeHtml.svg";
-import IconeCss from "../iconeCss.svg";
-import IconeJs from "../iconeJavaScript.svg";
-import IconeReact from "../iconeReact.svg";
+import SobreMimData from "../data/SobreMimData";
 import Card from "../components/Card";
 
 const SobreMim = () => {
@@ -15,68 +10,63 @@ const SobreMim = () => {
       <main>
         <section className="SobreMim">
           <div className="linha">
-            <h1>Sobre Mim</h1>
+            <h1>{SobreMimData.titulo}</h1>
             <div className="barra"></div>
             <p className="descricao">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
-              laborum provident consequuntur facilis inventore quis perspiciatis
-              enim et dolorem deserunt animi assumenda, doloremque, vero,
-              officiis nisi commodi alias architecto nam?
+              {SobreMimData.descricao}
             </p>
           </div>
+          
           <div className="linha">
             <h2>O Que Estou Fazendo</h2>
             <div className="coluna">
-              <section className="informacao">
-                <div className="conteudo">
-                  <img src={IconeDev} alt="icone" />
-                  <div className="text">
-                    <h3>Web Design</h3>
-                    <p className="descricao">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Error nesciunt possimus perferendis dolores, re
-                    </p>
+              {SobreMimData.atividades.map((atividade) => (
+                <section key={atividade.id} className="informacao">
+                  <div className="conteudo">
+                    <img src={atividade.icone} alt="icone" />
+                    <div className="text">
+                      <h3>{atividade.titulo}</h3>
+                      <p className="descricao">
+                        {atividade.descricao}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </section>
+                </section>
+              ))}
             </div>
           </div>
+          
           <div className="linha">
             <h2>Futuras Ideias</h2>
             <div className="coluna">
-              <section className="informacao">
-                <div className="blocos">
-                  <div className="blocoMaior">
-                    <img src={IconeMais} alt="Algo" />
+              {SobreMimData.futurasIdeias.map((ideia) => (
+                <section key={ideia.id} className="informacao">
+                  <div className="blocos">
+                    <div className="blocoMaior">
+                      <img src={ideia.icone} alt="Ideia futura" />
+                    </div>
+                    <div className="retangulos">
+                      <div className="forma1"></div>
+                      <div className="forma2"></div>
+                      <div className="forma2"></div>
+                    </div>
                   </div>
-                  <div className="retangulos">
-                    <div className="forma1"></div>
-                    <div className="forma2"></div>
-                    <div className="forma2"></div>
-                  </div>
-                </div>
-              </section>
-              <section className="informacao">
-                <div className="blocos">
-                  <div className="blocoMaior">
-                    <img src={IconeMais} alt="Algo" />
-                  </div>
-                  <div className="retangulos">
-                    <div className="forma1"></div>
-                    <div className="forma2"></div>
-                    <div className="forma2"></div>
-                  </div>
-                </div>
-              </section>
+                </section>
+              ))}
             </div>
           </div>
+          
           <div className="linha">
             <h2>Tecnologias</h2>
             <div className="coluna">
-              <img className="tecnologiasImg" src={IconeHtml} alt="HTML" />
-              <img className="tecnologiasImg" src={IconeJs} alt="JAVASCRIPT" />
-              <img className="tecnologiasImg" src={IconeCss} alt="CSS" />
-              <img className="tecnologiasImg" src={IconeReact} alt="REACT" />
+              {SobreMimData.tecnologias.map((tech) => (
+                <img 
+                  key={tech.id}
+                  className="tecnologiasImg" 
+                  src={tech.icone} 
+                  alt={tech.nome} 
+                />
+              ))}
             </div>
           </div>
         </section>
