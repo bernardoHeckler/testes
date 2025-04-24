d4e97b399e233657ccd3b98285fc45191cd0cbb9
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-04-24 16:20:09 -0300

    add: criando arquivo data para armazenar textos, imagens, titulos, e descriçoes que podem ser alterada via base de dados, sem mexer nos códigos.

---
+++ src/data/BlogData.jsx
@@ -0,0 +1,39 @@
+const BlogData = {
+    titulo: "Blog",
+    posts: [
+      {
+        id: 1,
+        imagem: "https://placehold.co/300x150/png",
+        categoria: "Design",
+        data: "Fev 23, 2022",
+        titulo: "Design Conference in 2022",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, nesciunt."
+      },
+      {
+        id: 2,
+        imagem: "https://placehold.co/300x150/png",
+        categoria: "Desenvolvimento",
+        data: "Mar 15, 2022",
+        titulo: "Novas Tendências em React",
+        descricao: "Descubra as últimas atualizações do React 18 e como utilizá-las."
+      },
+      {
+        id: 3,
+        imagem: "https://placehold.co/300x150/png",
+        categoria: "UI/UX",
+        data: "Abr 5, 2022",
+        titulo: "Design Acessível para Todos",
+        descricao: "Como criar interfaces inclusivas que atendam a diversos públicos."
+      },
+      {
+        id: 4,
+        imagem: "https://placehold.co/300x150/png",
+        categoria: "Nova Categoria",
+        data: "Mês Dia, Ano",
+        titulo: "Título do Novo Post",
+        descricao: "Descrição completa do novo post..."
+      }
+    ]
+  };
+  
+  export default BlogData;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../components/Card";
+import BlogData from "../data/BlogData";
 import "./Blog.css";
 
 const Blog = () => {
@@ -9,52 +10,25 @@ const Blog = () => {
       <main className="blog-container">
         <section className="blog-section">
           <div className="linha">
-            <h2>Blog</h2>
+            <h2>{BlogData.titulo}</h2>
             <div className="barra"></div>
           </div>
           <div className="linha">
-            <section className="blog-informacao">
-              <img
-                src="https://placehold.co/300x150/png"
-                alt="Placeholder genérico"
-              />
-              <div className="text">
-                <p className="infoData">Design - Fab 23, 2022</p>
-                <h4 className="tituloData">Design Conference in 2022</h4>
-                <p className="descricao">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Suscipit, nesciunt.
-                </p>
-              </div>
-            </section>
-            <section className="blog-informacao">
-              <img
-                src="https://placehold.co/300x150/png"
-                alt="Placeholder genérico"
-              />
-              <div className="text">
-                <p className="infoData">Design - Fab 23, 2022</p>
-                <h4 className="tituloData">Design Conference in 2022</h4>
-                <p className="descricao">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Suscipit, nesciunt.
-                </p>
-              </div>
-            </section>
-            <section className="blog-informacao">
-              <img
-                src="https://placehold.co/300x150/png"
-                alt="Placeholder genérico"
-              />
-              <div className="text">
-                <p className="infoData">Design - Fab 23, 2022</p>
-                <h4 className="tituloData">Design Conference in 2022</h4>
-                <p className="descricao">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Suscipit, nesciunt.
-                </p>
-              </div>
-            </section>
+            {BlogData.posts.map((post) => (
+              <section key={post.id} className="blog-informacao">
+                <img
+                  src={post.imagem}
+                  alt={`Imagem do post: ${post.titulo}`}
+                />
+                <div className="text">
+                  <p className="infoData">{post.categoria} - {post.data}</p>
+                  <h4 className="tituloData">{post.titulo}</h4>
+                  <p className="descricao">
+                    {post.descricao}
+                  </p>
+                </div>
+              </section>
+            ))}
           </div>
         </section>
       </main>
